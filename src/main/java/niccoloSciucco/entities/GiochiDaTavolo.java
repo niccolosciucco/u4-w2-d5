ab38ba52f7956cf6fd23f812f6b0d2e04b1de904
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-06-12 09:52:46 +0200

    extended abstract class

--- src/main/java/niccoloSciucco/entities/GiochiDaTavolo.java
+++ src/main/java/niccoloSciucco/entities/GiochiDaTavolo.java
@@ -1,10 +1,11 @@
 package niccoloSciucco.entities;
 
-public class GiochiDaTavolo {
+public class GiochiDaTavolo extends Gioco {
     private int numeroDiGiocatori;
     private double durataMediaPartita;
 
-    public GiochiDaTavolo(int numeroDiGiocatori, double durataMediaPartita) {
+    public GiochiDaTavolo(int idGioco, String titolo, int annoDiPubblicazione, double prezzo, int numeroDiGiocatori, double durataMediaPartita) {
+        super(idGioco, titolo, annoDiPubblicazione, prezzo);
         this.numeroDiGiocatori = numeroDiGiocatori;
         this.durataMediaPartita = durataMediaPartita;
     }
--- src/main/java/niccoloSciucco/entities/VideoGiochi.java
+++ src/main/java/niccoloSciucco/entities/VideoGiochi.java
@@ -2,12 +2,13 @@ package niccoloSciucco.entities;
 
 import niccoloSciucco.enums.Genere;
 
-public class VideoGiochi {
+public class VideoGiochi extends Gioco {
     private String piattaforma;
     private double durataDiGioco;
     private Genere genere;
 
-    public VideoGiochi(String piattaforma, double durataDiGioco, Genere genere) {
+    public VideoGiochi(int idGioco, String titolo, int annoDiPubblicazione, double prezzo, String piattaforma, double durataDiGioco, Genere genere) {
+        super(idGioco, titolo, annoDiPubblicazione, prezzo);
         this.piattaforma = piattaforma;
         this.durataDiGioco = durataDiGioco;
         this.genere = genere;
